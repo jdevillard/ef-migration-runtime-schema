492c62a4b4e02eaa337acc235eab928e994fc23f
jdevillard <djay93@hotmail.com>
2024-08-09 15:59:30 +0200

    Fix EnsureSchema method replacement (#5)
    * Refactor and Add Tests
    
    * Add schema to EnsureSchema method #4
    
    * update Package Version
    
    ---------
    
    Co-authored-by: Jérémie DEVILLARD <jdevillard@users.noreply.github.com>

--- src/ef-migration-runtimeschema/SchemaRewriter.cs
+++ src/ef-migration-runtimeschema/SchemaRewriter.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq.Expressions;
 
 namespace JDEV.EFMigrationRuntimeSchema
 {
@@ -8,15 +9,35 @@ namespace JDEV.EFMigrationRuntimeSchema
     {
         public override SyntaxNode? VisitArgument(ArgumentSyntax node)
         {
+            //Replace Schema for Up and Down
             if (node.NameColon?.Name.Identifier.Text == "schema")
-                return SyntaxFactory.Argument(
-                                                            SyntaxFactory.MemberAccessExpression(
-                                                                SyntaxKind.SimpleMemberAccessExpression,
-                                                                SyntaxFactory.IdentifierName("_schema"),
-                                                                SyntaxFactory.IdentifierName("Schema")))
-                    .WithNameColon(node.NameColon);
+                return CreateSchemaRefnode(node);
 
+
+            //Replace Schema Name for migrationBuilder.EnsureSchema()
+            if (node.NameColon?.Name.Identifier.Text == "name" 
+                && node.Parent != null
+                && node.Parent.Parent.IsKind(SyntaxKind.InvocationExpression))
+            {
+                var methodNode = node.Parent.Parent.DescendantNodesAndSelf().OfType<InvocationExpressionSyntax>().First();
+                var nameMethod = (methodNode.Expression as MemberAccessExpressionSyntax)?.Name;
+
+                if(nameMethod?.Identifier.Text =="EnsureSchema")
+                    return CreateSchemaRefnode(node);
+            }
+            
             return base.VisitArgument(node);
         }
+
+        private static SyntaxNode CreateSchemaRefnode(ArgumentSyntax node)
+        {
+            return SyntaxFactory.Argument(
+                                                                        SyntaxFactory.MemberAccessExpression(
+                                                                            SyntaxKind.SimpleMemberAccessExpression,
+                                                                            SyntaxFactory.IdentifierName("_schema"),
+                                                                            SyntaxFactory.IdentifierName("Schema")))
+                                .WithNameColon(node.NameColon);
+        }
+
     }
 }
